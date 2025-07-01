602d2f59ecdbf6db656d3fae95eb8cac18338219
circle33 <newnash@163.com>
2025-07-01 12:30:25 +0800

    fix: buffer

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
+import { Buffer } from 'buffer'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+
+// 设置全局 Buffer
+;(globalThis as any).Buffer = Buffer
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
