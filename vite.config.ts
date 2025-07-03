e01c65f4525997d02100be9e28d46a74c022a39e
circle33 <newnash@163.com>
2025-07-03 09:15:14 +0800

    revert

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
   define: {
     global: 'globalThis',
   },
